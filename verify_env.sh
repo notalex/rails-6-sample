528f95c1476428050c218fdcdd82ceb0c81c7f45
Alex J <alexj@synopsys.com>
2024-02-07 18:08:14 +0530

    temp

--- verify_env.sh
+++ verify_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-lines_changed_for_staging=$(git diff master HEAD *.env.staging | grep '^[+-][^+-]' | wc -l)
-lines_changed_for_production=$(git diff master HEAD *.env.production  | grep '^[+-][^+-]' | wc -l)
+lines_changed_for_staging=$(git diff origin/master HEAD *.env.staging | grep '^[+-][^+-]' | wc -l)
+lines_changed_for_production=$(git diff origin/master HEAD *.env.production  | grep '^[+-][^+-]' | wc -l)
 [ $lines_changed_for_staging == $lines_changed_for_production ]
