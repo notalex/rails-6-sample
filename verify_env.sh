0b15a189e004caaa5e1b65635c256c269e8e15d3
Alex J <alexj@synopsys.com>
2024-02-28 15:21:16 +0530

    Update Github workflow for ENV file verification

--- verify_env.sh
+++ verify_env.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-lines_changed_for_staging=$(git diff origin/master HEAD *.env.staging | grep '^[+-][^+-]' | wc -l)
-lines_changed_for_production=$(git diff origin/master HEAD *.env.production  | grep '^[+-][^+-]' | wc -l)
-[ $lines_changed_for_staging == $lines_changed_for_production ]
+branch=master
+
+if [[ $(git diff origin/$branch HEAD *.env.staging) ]]; then
+  changed_stg_files=$(git diff --name-only origin/$branch HEAD *.env.staging)
+  failed=
+
+  for stg_filename in $changed_stg_files; do
+    prd_filename=$(echo $stg_filename | sed 's/staging/production/')
+
+    stg_keys=$(cat $stg_filename | sed -E 's/^([^=]+).+/\1/' | sort)
+    prd_keys=$(cat $prd_filename | sed -E 's/^([^=]+).+/\1/' | sort)
+
+    ! [[ "${prd_keys[@]}" =~ "${stg_keys[@]}" ]] && echo "$prd_filename keys dont match with $stg_filename" && failed=1
+  done
+
+  [ $failed ] && echo 'ENV check Failed' && exit 1
+  exit 0
+fi
